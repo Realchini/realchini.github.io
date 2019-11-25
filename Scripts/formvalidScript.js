a0af4dda1df777e10721076bf36c6b8acad275ef
Realchini <realchini@users.noreply.github.com>
2019-11-25 12:26:51 +0200

    Update formvalidScript.js

--- Scripts/formvalidScript.js
+++ Scripts/formvalidScript.js
@@ -1,9 +1,18 @@
 function valid(form)
 {
     var topic = form.topic.value;
+    var message = form.message.value;
+    var email = form.email.value;
+    var phone = form.phone.value;
     var fail = false;
     if(!topic || topic == " ")
         fail = "Вы не ввели тему сообщения";
+    else if(!message || message == " ")
+        fail = "Вы не ввели сообщение";
+    else if(!email || email == " ")
+        fail = "Вы не ввели емейл";
+    else if(!phone || phone == " ")
+        fail = "Вы не ввели номер телефона";
     if(fail)
         {
             alert(fail);
