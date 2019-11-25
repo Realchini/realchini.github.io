b533c72cdeb26a9d60b8e4801a657d780e013a9f
Realchini <realchini@users.noreply.github.com>
2019-11-25 13:21:34 +0200

    drgdrgrd

--- Scripts/formvalidScript.js
+++ Scripts/formvalidScript.js
@@ -4,6 +4,9 @@ function valid(form)
     var message = form.message.value;
     var email = form.email.value;
     var phone = form.phone.value;
+    var email_pattern = /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/;
+    var phone_pattern = /^\+\d{2}\(\d{3}\)\d{3}-\d{2}-\d{2}$/;
+
     var fail = false;
     if(!topic || topic == " ")
         fail = "Вы не ввели тему сообщения";
@@ -11,8 +14,12 @@ function valid(form)
         fail = "Вы не ввели сообщение";
     else if(!email || email == " ")
         fail = "Вы не ввели емейл";
+    else if(email_pattern.test(email) == false)
+        fail = "Емейл неверного формата";
     else if(!phone || phone == " ")
         fail = "Вы не ввели номер телефона";
+    else if(phone_pattern.test(phone) == false)
+        fail = "Номер телефона неверного формата";
     if(fail)
         {
             alert(fail);
@@ -20,6 +27,8 @@ function valid(form)
         }
     else
         {
+            alert("Готово!");
+            window.location = "realchini.github.io/index.html"
             return true;
         }
 }
